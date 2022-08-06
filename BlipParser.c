2f7f8b030f2f74c4243fe9a47aec8813f45f7b20
wuyuyang0703 <99558234+wuyuyang0703@users.noreply.github.com>
2022-08-06 10:38:32 -0500

    Second Commit

--- BlipParser.c
+++ BlipParser.c
@@ -11,14 +11,26 @@ BlipToken::BlipToken(std::string data) {
     this->data = data;
 }
 
+TokenType BlipToken::type() {
+    if (CommandMap.find(data) != CommandMap.end()) {
+        return COMMAND;
+    }
+}
+
+std::istream& operator>> (std::istream& is, BlipToken& token) {
+    is >> token.data;
+    return is;
+}
+
 ErrorType BlipParser::loadFile(std::string fileName) {
     std::ifstream(fileName) ifs;
     if (!ifs.is_open()) {
         std::cerr << "Cannot Open File" << std::endl;
         return FILEERR;
     }
-    std::string command;
+    BlipToken command;
     std:::streampos orig = ifs.tellg();
     while (ifs >> command) {
-    }
+        
+    } 
 }
--- BlipParser.h
+++ BlipParser.h
@@ -2,16 +2,20 @@
 #include <string>
 
 enum TokenType {
-  COMMAND, NUMBER, OPERATOR, STRING
+  COMMAND, NUMBER, OPERATOR, LETTER, STRING
 };
 
 enum CommandType {
   TEXT, OUT, VAR, SET
-}
+};
 
 enum ErrorType {
     NOERR, FILEERR, COMMANDERR, ARGERR
-}
+};
+
+enum OperatorType {
+    ADD, SUB, MUL, DIV, MOD, AND, OR, LT, GT, EQ, NE, LE, GE, NOT, OPP
+};
 
 const std::unordered_map<std::string, CommandType> CommandMap = {
     {"text", TEXT},
@@ -20,13 +24,31 @@ const std::unordered_map<std::string, CommandType> CommandMap = {
     {"set", SET}
 };
 
+const std::unordered_map
+
 class BlipToken {
+private:
+    bool isNumber() {
+        for (std::string::iterator itr = data.begin(); itr != data.end(); ++itr) {
+            if (*itr > '9' || *itr < '0') return false;
+        }
+        return true;
+    }
+    bool isLetter() {
+        for (std::string::iterator itr = data.begin(); itr != data.end(); ++itr) {
+            if (*itr > 'z' || *itr < 'A') return false;
+        }
+        return true;
+    }
+    bool isOperator() {
+        if
+    }
 public:
-  std::string data;
-  TokenType type;
-  BlipToken();
-  BlipToken(std::string data);
-};
+    std::string data;
+    BlipToken();
+    BlipToken(std::string data);
+    TokenType type();
+    friend std::istream& operator>> (std::istream& is, BlipToken& token);
 
 class BlipParser {
 private:
